c896e6559e1ae865a21ae2c49d7add535326f4b7
unknown <diogo.torres.30@gmail.com>
2019-02-25 18:58:47 +0000

    ladida

--- testng/src/test/java/ap/TestCalculator.java
+++ testng/src/test/java/ap/TestCalculator.java
@@ -3,10 +3,10 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class TestCalculator {
-    
+    private Calculator ladida;
     @Test
     public void sumTestCalculator() {
-        public ladida = new Calculator("casio");    
+        ladida = new Calculator("casio");    
         System.out.println(ladida.sum(3,4));
-        }
+    }
 }
