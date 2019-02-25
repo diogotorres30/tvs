2baef25d3aa87a8418c1ab962e30ab950ef2183a
unknown <diogo.torres.30@gmail.com>
2019-02-25 18:46:07 +0000

    test

--- testng/src/test/java/ap/TestCalculator.java
+++ testng/src/test/java/ap/TestCalculator.java
@@ -1,9 +1,10 @@
-package src.main.java.ap;
+package ap;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class TestCalculator {
     @Test
     public void sumTestCalculator() {
+        System.out.println(Calculator.sum(3,4));
         }
 }
