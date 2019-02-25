aeadeb731c37082056d6fd6605c5f0f8064721a7
unknown <diogo.torres.30@gmail.com>
2019-02-25 18:37:04 +0000

    dsa

---
+++ testng/src/test/java/ap/TestCalculator.java
@@ -0,0 +1,11 @@
+package src.main.java.ap;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class TestCalculator {
+    @Test
+    public void sumTestCalculator() {
+        Calculator.sum();    
+        assertEquals()
+        }
+}
